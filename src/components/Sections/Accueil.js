d343d404c79fa6b1638e2a14137f4447db9e870d
Sonny Lallier <sonny.lallier@aios.sh>
2022-03-02 18:08:03 +0100

    ajout texte accueil, apropos, bts + update css

--- src/components/Sections/Accueil.js
+++ src/components/Sections/Accueil.js
@@ -4,7 +4,7 @@ export default function Accueil({ scrollToNextSection }) {
     return (
         <div className="accueil">
             <h1>Sonny LALLIER</h1>
-            <p>Bonjour, bienvenue sur mon super Portfolio !</p>
+            <p>Benvenue sur mon Portfolio !</p>
             <div className="wrapper-scroll" onClick={scrollToNextSection}>
                 <span className="scroll-icon">
                     <span className="scroll-icon__dot"></span>
