903b5d155552480a631d96ee15260dc96ec80cd1
Sonny <sonny.lepapa@gmail.com>
2022-03-14 12:34:52 +0100

    ajout de la section BTS SIO

--- src/components/Sections/BTS/BTS.tsx
+++ src/components/Sections/BTS/BTS.tsx
@@ -1,4 +1,8 @@
 import React from 'react';
+
+import { BsCodeSlash, BsHddNetwork } from 'react-icons/bs';
+import { boldFirstChar } from '../../../Utils/Texte';
+
 import "./bts.scss";
 
 export default function BTS() {
@@ -6,48 +10,48 @@ export default function BTS() {
         <div className="btssio">
             <h1>Qu'est ce que le BTS SIO</h1>
             <p className="box">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. At
-                voluptatem ex reiciendis vitae, nostrum illum autem voluptas,
-                fugit, amet aliquam rerum maxime quod nobis. Veniam beatae sequi
-                ad unde suscipit.
+                Le {boldFirstChar('Brevet')} de {boldFirstChar('Technicien')} {boldFirstChar('Supérieur')} {boldFirstChar('Services')} {boldFirstChar('Informatiques')} aux {boldFirstChar('Organisations')} (<b>BTS SIO</b>) est une formation en informatique à destination des futurs administrateurs réseaux et développeurs web.
+                <br /><br />
+                La formation se décline en deux options : <b>SISR</b> & <b>SLAM</b>
             </p>
             <div className="wrapper-options">
-                <div className="option slam box">
-                    <h2>SLAM</h2>
+                <div className="option sisr box">
+                    <h2><BsHddNetwork /> SISR</h2>
                     <p>
-                        Lorem ipsum, dolor sit amet consectetur adipisicing
-                        elit. Dolorum ducimus culpa veritatis dolores,
-                        aspernatur mollitia excepturi sunt magnam a iure beatae
-                        sit doloribus quo omnis illo expedita, recusandae
-                        necessitatibus odio? Alias, itaque illo hic
-                        exercitationem iure laboriosam quod quo et ad placeat!
-                        Commodi, ratione. Aliquam quis magnam, enim nostrum
-                        soluta quod commodi nam consequuntur obcaecati inventore
-                        porro. Eaque, iure repudiandae! Culpa facilis
-                        consequuntur vitae, dolorum obcaecati, ipsum corrupti
-                        maxime quidem aspernatur ea soluta modi, sapiente nemo
-                        in nulla atque. Similique repellendus necessitatibus
-                        excepturi corrupti obcaecati quo reiciendis, laudantium
-                        sunt dicta.
+                        L’option {boldFirstChar('Solutions')} d’{boldFirstChar('Informatique')}, {boldFirstChar('Systèmes')} et {boldFirstChar('Réseaux')} forme des professionnels des réseaux et équipements informatiques (installation, maintenance, sécurité). En sortant d’un BTS SIO SISR, vous serez capables de gérer et d’administrer le réseau d’une société et d’assurer sa sécurité et sa maintenance.
+                    </p>
+                    <p>
+                        Les techniciens supérieurs en informatique option SISR, peuvent accéder aux métiers de :
+                        <ul>
+                            <li>Administrateur systèmes et réseaux</li>
+                            <li>Informaticien support et déploiement</li>
+                            <li>Pilote d’exploitation</li>
+                            <li>Support systèmes et réseaux</li>
+                            <li>Technicien d’infrastructure</li>
+                            <li>Technicien de production</li>
+                            <li>Technicien micro et réseaux</li>
+                        </ul>
                     </p>
                 </div>
-                <div className="option sisr box">
-                    <h2>SISR</h2>
+                <div className="option slam box">
+                    <h2><BsCodeSlash /> SLAM</h2>
                     <p>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                        Necessitatibus temporibus tenetur aliquam, facere
-                        ducimus obcaecati error repudiandae laudantium facilis
-                        minima, dolorum qui id? Dolores aut exercitationem at
-                        obcaecati, recusandae unde? Doloribus fugit officiis
-                        quidem ipsa, incidunt nam ipsam consequuntur laudantium
-                        natus quod adipisci magni accusamus molestiae, eaque sed
-                        cumque in eos quos, voluptates fugiat optio minima
-                        exercitationem recusandae! Voluptatem, voluptates! Vitae
-                        nisi beatae voluptatem nesciunt omnis, praesentium
-                        tenetur neque quo excepturi distinctio inventore fugiat,
-                        ex sint id iusto, non dolorem veritatis! Libero,
-                        laudantium perferendis reiciendis quas suscipit nesciunt
-                        voluptatibus tempora!
+                        L’option {boldFirstChar('Solutions')} {boldFirstChar('Logicielles')} et {boldFirstChar('Applications')} {boldFirstChar('Métiers')} forme des spécialistes des logiciels (rédaction d’un cahier des charges, formulation des besoins et spécifications, développement, intégration au sein de la société).
+                    </p>
+                    <p>
+                        Les techniciens supérieurs en informatique option slam, sont préparés aux métiers de :
+                        <ul>
+                            <li>Développeur d’applications informatiques</li>
+                            <li>Développeur informatique</li>
+                            <li>Analyste d’applications ou d’études</li>
+                            <li>Analyste programmeur</li>
+                            <li>Chargé d’études informatiques</li>
+                            <li>Informaticien d’études</li>
+                            <li>Programmeur analyste</li>
+                            <li>Programmeur d’applications</li>
+                            <li>Responsable des services applicatifs</li>
+                            <li>Technicien d’études informatiques</li>
+                        </ul>
                     </p>
                 </div>
             </div>
