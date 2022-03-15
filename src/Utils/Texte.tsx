ee234f0da9fbebae59a3f59306670cab0263fa8f
Sonny <sonnyasdev@gmail.com>
2022-03-15 16:43:02 +0100

    création component liste + ajout contenu section "à propos"

--- src/Utils/Texte.tsx
+++ src/Utils/Texte.tsx
@@ -1,8 +1,19 @@
 import React from "react";
 
+import { CgChevronDoubleRight } from 'react-icons/cg';
+
 export function boldFirstChar(str: string) {
     const splitStr = str.split('');
     return (
         <><b>{splitStr.shift()}</b>{splitStr}</>
     )
+}
+
+
+export function List({ items }: { items: string[]; }) {
+    return <ul className="reset">
+        {items.map((item: any, key: number) => (
+            <li key={key}><CgChevronDoubleRight /> {item}</li>
+        ))}
+    </ul>;
 }
