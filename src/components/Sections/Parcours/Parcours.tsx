3e9916d815904f6497be3904f6155e5cc30ba466
Sonny <sonnyasdev@gmail.com>
2022-03-22 01:56:40 +0100

    wip: ajout parcours

--- src/components/Sections/Parcours/Parcours.tsx
+++ src/components/Sections/Parcours/Parcours.tsx
@@ -1,10 +1,48 @@
-import React from 'react';
+import React from "react";
+
+import "./parcours.scss";
+import formations from "./formations.json";
+
+interface FormationProps {
+    dateDebut: string;
+    dateFin: string;
+    ville: string;
+    cp: number;
+    etablissement: string;
+    details: string;
+}
 
 export default function Parcours() {
     return (
         <div className="parcours">
-            <h1>Mon super parcours !</h1>
-            <p className="box">Ceci est mon parcours</p>
+            <div className="formations">
+                <h1>Formations</h1>
+                <div className="timeline-container">
+                    <div className="timeline"></div>
+                    <ul className="reset">
+                        {formations.map(({ dateDebut, dateFin, ville, cp, etablissement, details }: FormationProps, key: number) => {
+                            return (
+                                <li key={key} className="item">
+                                    <div className="label box">
+                                        {details}
+                                    </div>
+                                    <div className="details">
+                                        <div className="date">
+                                            {dateDebut} - {dateFin}
+                                        </div>
+                                        <div className="location">
+                                            {etablissement} — {ville} ({cp})
+                                        </div>
+                                    </div>
+                                </li>
+                            )
+                        })}
+                    </ul>
+                </div>
+            </div>
+            <div className="experiences-pro">
+                <h2>Expériences professionnelles</h2>
+            </div>
         </div>
     );
 }
