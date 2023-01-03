6a030323b13b26ec60fe9190728b89b13d49e7f5
Sonny <sonnyasdev@gmail.com>
2023-01-03 11:52:05 +0100

    Edit: project list

--- src/types.d.ts
+++ src/types.d.ts
@@ -7,7 +7,17 @@ export interface Section {
 
 export interface SocialNetwork {
     id: 'github' | 'linkedin' | 'discord' | 'email';
+    id: "github" | "linkedin" | "discord" | "email";
     title: string;
     icon: any;
     link: string;
 }
+interface Projet {
+    nom: string;
+    description: string;
+    url: string;
+    thumbnail: string;
+    languages: string[];
+    disabled?: boolean;
+}
+
