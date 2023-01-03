750ee5347a521455ab3a8a25be943814a883b4ea
Sonny <sonnyasdev@gmail.com>
2023-01-03 14:38:36 +0100

    Edit: titles "about me"

--- src/components/Sections/APropos/APropos.tsx
+++ src/components/Sections/APropos/APropos.tsx
@@ -1,54 +1,76 @@
-import React from 'react';
+import React from "react";
 
-import { name } from '../../../config';
 import "./apropos.scss";
 
-import { BsCodeSlash } from 'react-icons/bs';
 import {
-    SiHtml5,
+    SiCsharp,
     SiCss3,
-    SiSass,
-    SiJavascript,
-    SiReact,
-    SiNextdotjs,
     SiExpress,
     SiFastify,
+    SiHtml5,
+    SiJavascript,
+    SiMysql,
+    SiNextdotjs,
     SiPhp,
     SiPython,
-    SiCsharp,
-    SiMysql
-} from 'react-icons/si';
+    SiReact,
+    SiSass,
+} from "react-icons/si";
 
 export default function APropos() {
     return (
         <div className="about-me">
             <h2>Mes compétences</h2>
-            <div className="box competences">
+            <div className="competences">
                 <div className="row">
-                    <h3><BsCodeSlash /> Languages Front End</h3>
+                    <h3>&#60;FrontEnd /&#62;</h3>
                     <ul className="reset languages lang-fe">
-                        <li><SiHtml5 /> HTML</li>
-                        <li><SiCss3 /> CSS</li>
-                        <li><SiSass /> SASS</li>
-                        <li><SiJavascript /> JavaScript</li>
-                        <li><SiReact /> React</li>
+                        <li>
+                            <SiHtml5 /> HTML
+                        </li>
+                        <li>
+                            <SiCss3 /> CSS
+                        </li>
+                        <li>
+                            <SiSass /> SASS
+                        </li>
+                        <li>
+                            <SiJavascript /> JavaScript
+                        </li>
+                        <li>
+                            <SiReact /> React
+                        </li>
                     </ul>
                 </div>
                 <div className="row">
-                    <h3><BsCodeSlash /> Languages Back End</h3>
+                    <h3>&#60;BackEnd /&#62;</h3>
                     <ul className="reset languages lang-fe">
-                        <li><SiNextdotjs /> Next</li>
-                        <li><SiExpress /> Express</li>
-                        <li><SiFastify /> Fastify</li>
-                        <li><SiPhp /> PHP</li>
+                        <li>
+                            <SiNextdotjs /> Next
+                        </li>
+                        <li>
+                            <SiExpress /> Express
+                        </li>
+                        <li>
+                            <SiFastify /> Fastify
+                        </li>
+                        <li>
+                            <SiPhp /> PHP
+                        </li>
                     </ul>
                 </div>
                 <div className="row">
-                    <h3><BsCodeSlash /> Autres</h3>
+                    <h3>&#60;Autres /&#62;</h3>
                     <ul className="reset languages lang-fe">
-                        <li><SiPython /> Python</li>
-                        <li><SiCsharp /> C#</li>
-                        <li><SiMysql /> MySQL</li>
+                        <li>
+                            <SiPython /> Python
+                        </li>
+                        <li>
+                            <SiCsharp /> C#
+                        </li>
+                        <li>
+                            <SiMysql /> MySQL
+                        </li>
                     </ul>
                 </div>
             </div>
