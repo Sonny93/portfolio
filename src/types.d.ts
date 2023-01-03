d66f76c3c665cb9dfd52dcb7fb979be27cfd1a29
Sonny <sonnyasdev@gmail.com>
2023-01-03 11:53:29 +0100

    Edit: formations & experiences

--- src/types.d.ts
+++ src/types.d.ts
@@ -3,15 +3,16 @@ export interface Section {
     label: string;
     background: string;
     component: any;
+    disabled?: boolean;
 }
 
 export interface SocialNetwork {
-    id: 'github' | 'linkedin' | 'discord' | 'email';
     id: "github" | "linkedin" | "discord" | "email";
     title: string;
     icon: any;
     link: string;
 }
+
 interface Projet {
     nom: string;
     description: string;
@@ -21,3 +22,23 @@ interface Projet {
     disabled?: boolean;
 }
 
+interface Formation {
+    dateDebut: string;
+    dateFin: string;
+    duree?: number;
+    ville: string;
+    codePostal: number;
+    etablissement: string;
+    details: string;
+}
+
+interface Experience {
+    dateDebut: string;
+    dateFin: string;
+    shortDate: string;
+    ville: string;
+    codePostal: number;
+    entreprise: string;
+    duree: number;
+    details: string;
+}
