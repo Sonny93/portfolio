4b72a31fe68b0070750ea5d65a4465bf321b10ac
Sonny <sonnyasdev@gmail.com>
2023-01-03 11:53:29 +0100

    Edit: formations & experiences

--- src/components/Sections/Parcours/Parcours.tsx
+++ src/components/Sections/Parcours/Parcours.tsx
@@ -1,62 +1,41 @@
-import React from 'react';
+import React from "react";
+import { CgChevronDoubleRight } from "react-icons/cg";
 
-import { CgChevronDoubleRight } from 'react-icons/cg';
+import experiences from "./experiences.json";
+import formations from "./formations.json";
 
-import './parcours.scss';
-import formations from './formations.json';
-import experiences from './experiences.json';
+import { Experience, Formation } from "../../../types";
 
-interface FormationProps {
-    dateDebut: string;
-    dateFin: string;
-    duree?: number;
-    ville: string;
-    codePostal: number;
-    etablissement: string;
-    details: string;
-}
-
-interface ExperiencesProps {
-    dateDebut: string;
-    dateFin: string;
-    shortDate: string;
-    ville: string;
-    codePostal: number;
-    entreprise: string;
-    duree: number;
-    details: string;
-}
+import "./parcours.scss";
 
 export default function Parcours() {
     return (
-        <div className='parcours'>
+        <div className="parcours">
             <h1>Mon parcours</h1>
-            <div className='timelines-wrapper'>
-                <div className='timeline-wrapper'>
+            <div className="timelines-wrapper">
+                <div className="timeline-wrapper">
                     <h2>Formations</h2>
-                    <div className='timeline-container'>
-                        <div className='timeline'></div>
-                        <ul className='reset'>
-                            {formations.map((formation: FormationProps, key: number) => (
-                                <FormationItem
-                                    key={key}
-                                    {...formation}
-                                />
-                            ))}
+                    <div className="timeline-container">
+                        <div className="timeline"></div>
+                        <ul className="reset">
+                            {formations.map(
+                                (formation: Formation, key: number) => (
+                                    <FormationItem key={key} {...formation} />
+                                )
+                            )}
                         </ul>
                     </div>
                 </div>
-                <div className='timeline-wrapper'>
+                <div className="timeline-wrapper">
                     <h2>Expériences professionnelles</h2>
-                    <div className='timeline-container'>
-                        <div className='timeline'></div>
-                        <ul className='reset'>
-                            {experiences.map((experience: ExperiencesProps, key: number) => (
-                                <ExperienceItem
-                                    key={key}
-                                    {...experience}
-                                />
-                            ))}
+                    <div className="timeline-container">
+                        <div className="timeline"></div>
+                        <ul className="reset">
+                            {experiences.map(
+                                (experience: Experience, key: number) => (
+                                    <ExperienceItem key={key} {...experience} />
+                                )
+                            )}
                         </ul>
                     </div>
                 </div>
@@ -65,38 +44,81 @@ export default function Parcours() {
     );
 }
 
-function FormationItem({ details, dateDebut, dateFin, duree, etablissement, ville, codePostal }: FormationProps) {
-    return (<>
-        <li className='item'>
-            <div className='label' title={details}>
-                {details}
-            </div>
-            <div className='details'>
-                <div className='date'>
-                    {dateDebut === dateFin ? dateDebut : <>{dateDebut} - {dateFin}</>} {duree && (<span style={{ color: '#aaa' }}> — {duree} an{duree > 1 ? 's' : ''}</span>)}
+function FormationItem({
+    details,
+    dateDebut,
+    dateFin,
+    duree,
+    etablissement,
+    ville,
+    codePostal,
+}: Formation) {
+    return (
+        <>
+            <li className="item">
+                <div className="label" title={details}>
+                    {details}
                 </div>
-                <div className='location'>
-                    <CgChevronDoubleRight /> {etablissement}<span style={{ color: '#aaa' }}> — {ville}, {codePostal}</span>
+                <div className="details">
+                    <div className="date">
+                        {dateDebut === dateFin ? (
+                            dateDebut
+                        ) : (
+                            <>
+                                {dateDebut} - {dateFin}
+                            </>
+                        )}{" "}
+                        {duree && (
+                            <span style={{ color: "#aaa" }}>
+                                {" "}
+                                — {duree} an{duree > 1 ? "s" : ""}
+                            </span>
+                        )}
+                    </div>
+                    <div className="location">
+                        <CgChevronDoubleRight /> {etablissement}
+                        <span style={{ color: "#aaa" }}>
+                            {" "}
+                            — {ville}, {codePostal}
+                        </span>
+                    </div>
                 </div>
-            </div>
-        </li>
-    </>);
+            </li>
+        </>
+    );
 }
 
-function ExperienceItem({ details, shortDate, duree, entreprise, ville, codePostal }: ExperiencesProps) {
-    return (<>
-        <li className='item'>
-            <div className='label' title={details}>
-                {details}
-            </div>
-            <div className='details'>
-                <div className='date'>
-                    {shortDate} <span style={{ color: '#aaa' }}> — {duree} semaines</span>
+function ExperienceItem({
+    details,
+    shortDate,
+    duree,
+    entreprise,
+    ville,
+    codePostal,
+}: Experience) {
+    return (
+        <>
+            <li className="item">
+                <div className="label" title={details}>
+                    {details}
                 </div>
-                <div className='location'>
-                    <CgChevronDoubleRight /> {entreprise}<span style={{ color: '#aaa' }}> — {ville}, {codePostal}</span>
+                <div className="details">
+                    <div className="date">
+                        {shortDate}{" "}
+                        <span style={{ color: "#aaa" }}>
+                            {" "}
+                            — {duree} semaines
+                        </span>
+                    </div>
+                    <div className="location">
+                        <CgChevronDoubleRight /> {entreprise}
+                        <span style={{ color: "#aaa" }}>
+                            {" "}
+                            — {ville}, {codePostal}
+                        </span>
+                    </div>
                 </div>
-            </div>
-        </li>
-    </>);
+            </li>
+        </>
+    );
 }
--- src/config.tsx
+++ src/config.tsx
@@ -1,82 +1,95 @@
-import React, { createContext } from 'react';
-import { BsDiscord, BsGithub, BsLinkedin } from 'react-icons/bs';
-import { IoMailOutline } from 'react-icons/io5';
+import React, { createContext } from "react";
+import { BsDiscord, BsGithub, BsLinkedin } from "react-icons/bs";
+import { IoMailOutline } from "react-icons/io5";
 
-import Accueil from './components/Sections/Accueil/Accueil';
-import APropos from './components/Sections/APropos/APropos';
-import BTS from './components/Sections/BTS/BTS';
-import Contact from './components/Sections/Contact/Contact';
-import Parcours from './components/Sections/Parcours/Parcours';
-import Projets from './components/Sections/Projets/Projets';
-import VeilleTechnologique from './components/Sections/VeilleTechno/Veille';
+import Accueil from "./components/Sections/Accueil/Accueil";
+import APropos from "./components/Sections/APropos/APropos";
+import BTS from "./components/Sections/BTS/BTS";
+import Contact from "./components/Sections/Contact/Contact";
+import Parcours from "./components/Sections/Parcours/Parcours";
+import Projets from "./components/Sections/Projets/Projets";
+import VeilleTechnologique from "./components/Sections/VeilleTechno/Veille";
 
-import { Section, SocialNetwork } from './types';
+import { Section, SocialNetwork } from "./types";
 
-export const PATH_BG_IMG = '/img/background';
-export const PATH_BG_PROJETS = '/img/projets';
+export const PATH_BG_IMG = "/img/background";
+export const PATH_BG_PROJETS = "/img/projets";
 
-export const name: string = 'Sonny';
-export const email: string = 'sonnylallier1@gmail.com';
+export const name: string = "Sonny";
+export const email: string = "sonnylallier1@gmail.com";
 
 export const socialNetworks: Array<SocialNetwork> = [
     {
-        id: 'github',
-        link: 'https://github.com/Sonny93',
+        id: "github",
+        link: "https://github.com/Sonny93",
         icon: <BsGithub />,
-        title: 'Page Github'
-    }, {
-        id: 'linkedin',
-        link: 'https://www.linkedin.com/in/sonnylallier/',
+        title: "Page Github",
+    },
+    {
+        id: "linkedin",
+        link: "https://www.linkedin.com/in/sonnylallier/",
         icon: <BsLinkedin />,
-        title: 'Page Linkedin'
-    }, {
-        id: 'discord',
-        link: 'https://discord.com/',
+        title: "Page Linkedin",
+    },
+    {
+        id: "discord",
+        link: "https://discord.com/",
         icon: <BsDiscord />,
-        title: 'Identifiant Discord : Sonny#0005'
-    }, {
-        id: 'email',
-        link: 'mailto:' + email,
+        title: "Identifiant Discord : Sonny#0005",
+    },
+    {
+        id: "email",
+        link: "mailto:" + email,
         icon: <IoMailOutline />,
-        title: 'Adresse email : ' + email
-    }
+        title: "Adresse email : " + email,
+    },
 ];
 
-export const sections = [{
-    name: 'accueil',
-    label: 'Accueil',
-    background: 'accueil-bg.webp',
-    component: Accueil
-}, {
-    name: 'aboutme',
-    label: 'À propos',
-    background: 'apropos-bg.webp',
-    component: APropos
-}, {
-    name: 'btssio',
-    label: 'BTS SIO',
-    background: 'bts-sio-bg.webp',
-    component: BTS
-}, {
-    name: 'parcours',
-    label: 'Mon parcours',
-    background: 'mon-parcours-bg.webp',
-    component: Parcours
-}, {
-    name: 'projets',
-    label: 'Mes projets',
-    background: 'mes-projets-bg.webp',
-    component: Projets
-}, {
-    name: 'veilletechno',
-    label: 'Veille Technologique',
-    background: 'veille-techno-bg.webp',
-    component: VeilleTechnologique
-}, {
-    name: 'contact',
-    label: 'Me contacter',
-    background: 'me-contacter-bg.webp',
-    component: Contact
-}] as Section[];
+export const sections: Section[] = [
+    {
+        name: "accueil",
+        label: "Accueil",
+        background: "accueil-bg.webp",
+        component: Accueil,
+    },
+    {
+        name: "aboutme",
+        label: "À propos",
+        background: "apropos-bg.webp",
+        component: APropos,
+    },
+    {
+        name: "btssio",
+        label: "BTS SIO",
+        background: "bts-sio-bg.webp",
+        component: BTS,
+        disabled: true,
+    },
+    {
+        name: "parcours",
+        label: "Mon parcours",
+        background: "mon-parcours-bg.webp",
+        component: Parcours,
+    },
+    {
+        name: "projets",
+        label: "Mes projets",
+        background: "mes-projets-bg.webp",
+        component: Projets,
+    },
+    {
+        name: "veilletechno",
+        label: "Veille Technologique",
+        background: "veille-techno-bg.webp",
+        component: VeilleTechnologique,
+        disabled: true,
+    },
+    {
+        name: "contact",
+        label: "Me contacter",
+        background: "me-contacter-bg.webp",
+        component: Contact,
+    },
+].filter((section) => !section?.disabled);
 
 export const SectionsProvider = createContext(sections);
