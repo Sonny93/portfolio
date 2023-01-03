84fb1993df50bd326de251d87c6cea081f9d52fb
Sonny <sonnyasdev@gmail.com>
2023-01-03 11:52:05 +0100

    Edit: project list

--- src/components/Sections/Projets/Projets.tsx
+++ src/components/Sections/Projets/Projets.tsx
@@ -1,40 +1,44 @@
 import React from "react";
 
-import projets from './projets.json';
+import { Projet } from "../../../types";
+import projetList from "./projets.json";
 import "./projets.scss";
 
-const PATH_IMAGES = '/img/projets';
-
-interface Projet {
-    nom: string;
-    description: string;
-    url: string;
-    thumbnail: string;
-    languages: string[];
-}
+const PATH_IMAGES = "/img/projets";
 
 export default function Projets() {
+    const projets = projetList.filter((projet: Projet) => !projet?.disabled);
     return (
         <div className="projets">
             <h1>Mes projets</h1>
             <p className="box">
-                Voici quelques uns de mes projets réalisés sur mon temps libre. Le code source de chacun des projets est sur Github (dans des repos privés) et sont hébergés chez Vercel ou sur mon VPS.
+                Voici quelques uns de mes projets réalisés sur mon temps libre.
+                Le code source de chacun des projets est sur Github (dans des
+                repos privés) et sont hébergés chez Vercel ou sur mon VPS.
             </p>
             <ul className="reset">
-                {projets.map(({ nom, description, url, thumbnail, languages }: Projet, key: number) => (
-                    <li className="projet" key={key}>
-                        <a href={url} target="_blank" rel="noreferrer">
-                            <img src={PATH_IMAGES + '/' + thumbnail} alt={`${nom} thumbnail`} />
-                            <div className="details">
-                                <span className="languages">{languages.join(", ")}</span>
-                                <span className="name">{nom}</span>
-                            </div>
-                        </a>
-                        <div className="description">
-                            {description}
-                        </div>
-                    </li>
-                ))}
+                {projets.map(
+                    (
+                        { nom, description, url, thumbnail, languages }: Projet,
+                        key: number
+                    ) => (
+                        <li className="projet" key={key}>
+                            <a href={url} target="_blank" rel="noreferrer">
+                                <img
+                                    src={PATH_IMAGES + "/" + thumbnail}
+                                    alt={`${nom} thumbnail`}
+                                />
+                                <div className="details">
+                                    <span className="languages">
+                                        {languages.join(", ")}
+                                    </span>
+                                    <span className="name">{nom}</span>
+                                </div>
+                            </a>
+                            <div className="description">{description}</div>
+                        </li>
+                    )
+                )}
             </ul>
         </div>
     );
