78579e0f184926456efe691883c8582b6b7257fc
Sonny <sonnyasdev@gmail.com>
2023-01-07 00:38:13 +0100

    Add: tilt parallax effect

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -1,13 +1,20 @@
 import React from "react";
+import Tilt from "react-parallax-tilt";
 
-export default function Avatar({ size }: { size: number; }) {
-    return (<>
-        <img
-            height={size}
-            width={size}
-            style={{ borderRadius: '50%', marginTop: '15px', boxShadow: '0 0 10px 1px rgba(0, 0, 0, .25)' }}
-            src="sept.jpg"
-            alt="avatar"
-        />
-    </>);
+export default function Avatar({ size }: { size: number }) {
+    return (
+        <Tilt>
+            <img
+                height={size}
+                width={size}
+                style={{
+                    borderRadius: "50%",
+                    marginTop: "15px",
+                    boxShadow: "0 0 10px 1px rgba(0, 0, 0, .25)",
+                }}
+                src="sept.jpg"
+                alt="avatar"
+            />
+        </Tilt>
+    );
 }
