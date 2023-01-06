25a74db1c9d4b6de585156ad12c0667c5be4b79e
Sonny <sonnyasdev@gmail.com>
2023-01-07 00:38:13 +0100

    Add: tilt parallax effect

--- src/components/Sections/APropos/APropos.tsx
+++ src/components/Sections/APropos/APropos.tsx
@@ -1,7 +1,4 @@
-import React from "react";
-
-import "./apropos.scss";
-
+import React, { ReactNode } from "react";
 import {
     SiCsharp,
     SiCss3,
@@ -16,8 +13,73 @@ import {
     SiReact,
     SiSass,
 } from "react-icons/si";
+import Tilt from "react-parallax-tilt";
+
+import "./apropos.scss";
+
+interface Langage {
+    label: string;
+    icon: JSX.Element;
+}
 
 export default function APropos() {
+    const frontLangages: Langage[] = [
+        {
+            label: "HTML",
+            icon: <SiHtml5 />,
+        },
+        {
+            label: "CSS",
+            icon: <SiCss3 />,
+        },
+        {
+            label: "SASS",
+            icon: <SiSass />,
+        },
+        {
+            label: "JavaScript",
+            icon: <SiJavascript />,
+        },
+        {
+            label: "React",
+            icon: <SiReact />,
+        },
+    ];
+
+    const backLangages: Langage[] = [
+        {
+            label: "Next",
+            icon: <SiNextdotjs />,
+        },
+        {
+            label: "Express",
+            icon: <SiExpress />,
+        },
+        {
+            label: "Fastify",
+            icon: <SiFastify />,
+        },
+        {
+            label: "PHP",
+            icon: <SiPhp />,
+        },
+    ];
+
+    const otherLangages: Langage[] = [
+        {
+            label: "Python",
+            icon: <SiPython />,
+        },
+        {
+            label: "C#",
+            icon: <SiCsharp />,
+        },
+        {
+            label: "MySQL",
+            icon: <SiMysql />,
+        },
+    ];
+
     return (
         <div className="about-me">
             <h2>Mes compétences</h2>
@@ -25,55 +87,39 @@ export default function APropos() {
                 <div className="row">
                     <h3>&#60;FrontEnd /&#62;</h3>
                     <ul className="reset languages lang-fe">
-                        <li>
-                            <SiHtml5 /> HTML
-                        </li>
-                        <li>
-                            <SiCss3 /> CSS
-                        </li>
-                        <li>
-                            <SiSass /> SASS
-                        </li>
-                        <li>
-                            <SiJavascript /> JavaScript
-                        </li>
-                        <li>
-                            <SiReact /> React
-                        </li>
+                        {frontLangages.map(BuildItem)}
                     </ul>
                 </div>
                 <div className="row">
                     <h3>&#60;BackEnd /&#62;</h3>
                     <ul className="reset languages lang-fe">
-                        <li>
-                            <SiNextdotjs /> Next
-                        </li>
-                        <li>
-                            <SiExpress /> Express
-                        </li>
-                        <li>
-                            <SiFastify /> Fastify
-                        </li>
-                        <li>
-                            <SiPhp /> PHP
-                        </li>
+                        {backLangages.map(BuildItem)}
                     </ul>
                 </div>
                 <div className="row">
                     <h3>&#60;Autres /&#62;</h3>
                     <ul className="reset languages lang-fe">
-                        <li>
-                            <SiPython /> Python
-                        </li>
-                        <li>
-                            <SiCsharp /> C#
-                        </li>
-                        <li>
-                            <SiMysql /> MySQL
-                        </li>
+                        {otherLangages.map(BuildItem)}
                     </ul>
                 </div>
             </div>
         </div>
+    );
+}
+
+function BuildItem({ label, icon }: Langage) {
+    return (
+        <LangageItem>
+            {icon}
+            {label}
+        </LangageItem>
+    );
+}
+
+function LangageItem({ children }: { children: ReactNode }) {
+    return (
+        <Tilt tiltReverse perspective={500}>
+            <li>{children}</li>
+        </Tilt>
     );
 }
