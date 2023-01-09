c5a07c522dfaff97b88eaa0ca2478d6be687a55b
Sonny <sonnyasdev@gmail.com>
2023-01-09 16:53:53 +0100

    Fix: prefetch background images

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,26 @@
-import React, { useEffect, useState } from 'react';
-import { Section } from './types';
+import React, { useEffect, useState } from "react";
+import { Section } from "./types";
 
 // Components
-import Navbar from './components/Navbar/Navbar';
-import SectionsList from './components/Sections/SectionsList';
+import Navbar from "./components/Navbar/Navbar";
+import SectionsList from "./components/Sections/SectionsList";
 
 // Config
-import { PATH_BG_IMG, sections, SectionsProvider } from './config';
+import { PATH_BG_IMG, sections, SectionsProvider } from "./config";
 
 export default function App() {
     const [activeSection, setActiveSection] = useState<Section>(sections[0]);
-    const [background, setBackground] = useState<string>(sections[0].background);
+    const [background, setBackground] = useState<string>(
+        sections[0].background
+    );
 
-    useEffect(() => sections.forEach(({ background }: Section) => PreloadImage(background)), []);
+    useEffect(
+        () =>
+            sections.forEach(({ background }: Section) =>
+                PreloadImage(background)
+            ),
+        []
+    );
     useEffect(() => {
         if (activeSection) {
             setBackground(activeSection.background);
@@ -20,17 +28,20 @@ export default function App() {
     }, [activeSection]);
 
     const changeActiveSection = (name: string) => {
-        const section = sections.find((s => s.name === name));
+        const section = sections.find((s) => s.name === name);
         if (section) {
             setActiveSection(section);
         } else {
-            console.warn('Unable to find active section');
+            console.warn("Unable to find active section");
         }
-    }
+    };
 
     return (
-        <div className='background' style={{ backgroundImage: `url(${PATH_BG_IMG}/${background})` }}>
-            <div className='App'>
+        <div
+            className="background"
+            style={{ backgroundImage: `url(${PATH_BG_IMG}/${background})` }}
+        >
+            <div className="App">
                 <SectionsProvider.Provider value={sections}>
                     <Navbar
                         setActiveSection={changeActiveSection}
@@ -44,10 +55,11 @@ export default function App() {
 }
 
 function PreloadImage(image: string): void {
-    const link = document.createElement('link');
-    link.setAttribute('rel', 'preload');
-    link.setAttribute('as', 'image');
-    link.setAttribute('crossorigin', 'anonymous');
-    link.setAttribute('href', image);
+    const link = document.createElement("link");
+    link.setAttribute("rel", "prefetch");
+    link.setAttribute("as", "image");
+    link.setAttribute("crossorigin", "anonymous");
+    link.setAttribute("importance", "high");
+    link.setAttribute("href", image);
     document.head.appendChild(link);
 }
